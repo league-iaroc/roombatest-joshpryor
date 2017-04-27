53e92bfce2c4008b8f143e6c70a2405513982cf3
League <league@WorthyMini.local>
2017-04-26 17:59:51 -0700

    fun...ish
    2 n 3

--- src/level2/Challenge.java
+++ src/level2/Challenge.java
@@ -13,11 +13,23 @@ public class Challenge extends Head {
 	public Challenge(Roomba roomba) {
 		super(roomba);
 		Sonar sonar = new Sonar(roomba);
+		int speed = 700 ;
 	}
 
 	public void initialize() {
+int speed = 700 ;
+driveDirect(speed,speed);
+sleep(2000);
+driveDirect(0,90);
+sleep(3200);
+driveDirect(speed,speed);
+sleep(3500);
+driveDirect(90,0);
+sleep(3000);
+driveDirect(speed,speed);
 
 	}
+	
 
 	public void loop() {
 	
--- src/level3/Challenge.java
+++ src/level3/Challenge.java
@@ -4,20 +4,45 @@ import processing.core.PApplet;
 
 public class Challenge extends Head {
 	Roomba roomba;
-
+	 static int speed = 700;
+	 static int three = 3000;
+	 static int two = 1800;
 	public static void main(String[] args) {
 		PApplet.main("level3.Processing");
 	}
 
 	public Challenge(Roomba roomba) {
 		super(roomba);
+			
 	}
 
 	public void initialize() {
-	
+		gofront(two);
+		leftTurn(90);
+		gofront(1500);
+		driveDirect(90,0);
+		sleep(3200);
+		gofront(1500);
+		leftTurn(90);
+		gofront(two);
+		driveDirect(90,0);
+		sleep(three);
+		gofront(9999999);
+		
 	}
 
 	public void loop() {
 	
+	}
+	
+	void leftTurn(int things){
+		driveDirect(0,things);
+		sleep(three);
+		
+	}
+	
+	void gofront( int time){
+		driveDirect(speed,speed);
+		sleep(time);
 	}
 }
